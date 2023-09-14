37d5323c9309a45ad3a98356ca04691ab1df0746
Fábio simones <simones.fabio1994@gmail.com>
2023-09-14 18:27:02 -0300

    Criando View CheckoutCompleto, exibindo resumo do pedido

--- LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/Controllers/PedidoController.cs
@@ -9,7 +9,8 @@ namespace LanchesMac.Controllers
         private readonly IPedidoRepository _pedidoRepository;
         private readonly CarrinhoCompra _carrinhoCompra;
 
-        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        public PedidoController(IPedidoRepository pedidoRepository,
+            CarrinhoCompra carrinhoCompra)
         {
             _pedidoRepository = pedidoRepository;
             _carrinhoCompra = carrinhoCompra;
@@ -27,45 +28,44 @@ namespace LanchesMac.Controllers
             int totalItensPedido = 0;
             decimal precoTotalPedido = 0.0m;
 
-            //Obtém os itens do carrinho de compra do cliente
+            //obtem os itens do carrinho de compra do cliente
             List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
             _carrinhoCompra.CarrinhoCompraItems = items;
 
-            //Verifica se existem itens de pedido
+            //verifica se existem itens de pedido
             if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
             {
-                ModelState.AddModelError("", "Seu carrinho está vazio, favor incluir um lanche...");
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
             }
 
             //calcula o total de itens e o total do pedido
-            foreach(var item in items)
+            foreach (var item in items)
             {
                 totalItensPedido += item.Quantidade;
                 precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
             }
 
-            //Atribui valores obtidos no pedido
+            //atribui os valores obtidos ao pedido
             pedido.TotalItensPedido = totalItensPedido;
             pedido.PedidoTotal = precoTotalPedido;
 
-            //Valida os dados do pedido
-            if(ModelState.IsValid)
+            //valida os dados do pedido
+            if (ModelState.IsValid)
             {
-                //Cria o pedido e os detalhes do pedido
+                //cria o pedido e os detalhes
                 _pedidoRepository.CriarPedido(pedido);
 
-                //Define mensagens ao cliente
-                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :D";
-                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraItens();
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
 
-                //Limpa o carrinho do cliente
+                //limpa o carrinho do cliente
                 _carrinhoCompra.LimparCarrinho();
 
-                //Exibe a view com dados do cliente e do pedido
+                //exibe a view com dados do cliente e do pedido
                 return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
             }
             return View(pedido);
         }
-
     }
 }
---
+++ LanchesMac/Migrations/20230914211142_AtualizaPedidos.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class AtualizaPedidos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Pedidos",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Pedidos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Pedidos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Pedidos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
