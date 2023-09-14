63a8f8f915f7b7cd0bf378fcf7bc37c14a972003
Fábio simones <simones.fabio1994@gmail.com>
2023-09-14 15:01:24 -0300

    Criando aba de pesquisa para localizar lanches

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -22,8 +22,10 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddTransient<IPedidoRepository, PedidoRepository>();
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        services.AddTransient<IPedidoRepository, PedidoRepository>(); 
+        
         services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
         services.AddControllersWithViews();
