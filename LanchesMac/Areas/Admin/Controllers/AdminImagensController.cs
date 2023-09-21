56fe5a406a595a51f18afc464e2ddd47c9c35280
Fábio simones <simones.fabio1994@gmail.com>
2023-09-21 19:19:46 -0300

    Exibindo opções para Upload e Consulta de imagens.

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public AdminImagensController(IOptions<ConfigurationImagens> myConfiguration, IWebHostEnvironment hostEnvironment)
+        {
+            _myConfig = myConfiguration.Value;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
