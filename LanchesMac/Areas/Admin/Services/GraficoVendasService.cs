e9569dd76ccb3187a96d618b72c448bdc87c5e5c
Fábio simones <simones.fabio1994@gmail.com>
2023-09-22 16:39:56 -0300

    Criando Controller AdminGraficoController.

--- LanchesMac/Areas/Admin/Services/GraficoVendasService.cs
+++ LanchesMac/Areas/Admin/Services/GraficoVendasService.cs
@@ -14,7 +14,7 @@ namespace LanchesMac.Areas.Admin.Services
 
         public List<LancheGrafico> GetVendasLanches(int dias=360)
         {
-            var data = DateTime.Now.AddDays(-360);
+            var data = DateTime.Now.AddDays(-dias);
             var lanches = (from pd in _appDbContext.PedidoDetalhes
                            join l in _appDbContext.Lanches on pd.LancheId equals l.LancheId
                            where pd.Pedido.PedidoEnviado >= data
